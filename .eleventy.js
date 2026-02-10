14a32e972480f84a96c5a31afe9cd220f7496dc2
Aaron Sullivan <aaron.n.sullivan@gmail.com>
2026-02-10 01:42:51 -0800

    Fix CMS uploads and copy images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
   // Use the relative path from the root
   eleventyConfig.addPassthroughCopy("./admin"); 
+  eleventyConfig.addPassthroughCopy("./images");
+  eleventyConfig.addPassthroughCopy("src/gallery/**/*.{png,jpg,jpeg,webp,gif,svg}");
   
   // Rest of your config...
   return {
