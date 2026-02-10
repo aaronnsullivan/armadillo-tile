6b7cc91bec797f4db3eaa80add0446cf0eb86aa0
Aaron Sullivan <aaron.n.sullivan@gmail.com>
2026-02-10 02:32:31 -0800

    Force CMS photos to root images folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,6 @@
 module.exports = function(eleventyConfig) {
-  // Copies the CMS admin panel
   eleventyConfig.addPassthroughCopy("./admin"); 
-  
-  // Copies the main images folder where the CMS is now saving photos
   eleventyConfig.addPassthroughCopy("./images");
-  
-  // Copies the src/gallery folder so Eleventy can find the .md data files
   eleventyConfig.addPassthroughCopy("src/gallery");
   
   return {
