3dad6b53ae5a38fc3efda6ecd4fdce645602bb88
Aaron Sullivan <aaron.n.sullivan@gmail.com>
2026-02-10 01:18:25 -0800

    Fix: add functions and update eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,8 @@
 module.exports = function(eleventyConfig) {
-  // 1. Copy the 'images' folder to the final site
-  eleventyConfig.addPassthroughCopy("images");
+  // Use the relative path from the root
+  eleventyConfig.addPassthroughCopy("./admin"); 
   
-  // 2. Copy the 'fonts' folder to the final site
-  eleventyConfig.addPassthroughCopy("fonts");
-  
-  // 3. (Important) Do you have a CSS file? 
-  // I don't see one in your screenshot root. 
-  // If it's inside a folder, copy that folder too.
-  // If it's named 'style.css' in the root, uncomment the line below:
-  // eleventyConfig.addPassthroughCopy("style.css");
-
+  // Rest of your config...
   return {
     dir: {
       input: ".",
