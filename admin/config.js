0c335d44cf7c349ce28a3a59fafb1a4ada914dc3
Aaron Sullivan <aaron.n.sullivan@gmail.com>
2026-02-10 21:35:17 -0800

    added .js

---
+++ admin/config.js
@@ -0,0 +1,29 @@
+const config = {
+  backend: {
+    name: 'github',
+    repo: 'aaronnsullivan/armadillo-tile',
+    branch: 'master'
+  },
+  display_url: 'https://armadillotile.com',
+  logo_url: 'https://armadillotile.com/images/armadillo.png',
+  media_folder: 'images',
+  public_folder: '/images',
+  collections: [
+    {
+      name: 'gallery',
+      label: 'Photos. Love Ya Bud. Nice Hog.',
+      folder: 'src/gallery',
+      create: true,
+      slug: '{{slug}}',
+      fields: [
+        {label: 'Title', name: 'title', widget: 'string'},
+        {label: 'Tags', name: 'tags', widget: 'hidden', default: ['gallery']},
+        {label: 'Image', name: 'image', widget: 'image'},
+        {label: 'Alt Text', name: 'alt', widget: 'string'},
+        {label: 'Body', name: 'body', widget: 'markdown', required: false}
+      ]
+    }
+  ]
+};
+
+CMS.init({ config });
